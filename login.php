efe6f0f82f643172b099f99e7ac1f79952b757b5
BenjiiXXV <benjaminwijnsma@gmail.com>
2017-02-20 14:24:43 +0100

    updated modal

--- login.php
+++ login.php
@@ -12,51 +12,69 @@
     <script src="bootjq/bootstrap.min.js"></script>
  </head>
 
-
 <div class="container">
-  <h2>Log in</h2>
-  <!-- Trigger the modal with a button -->
-  <button type="button" class="btn btn-info btn-lg" id="myBtn">Login</button>
-
-  <!-- Modal -->
+  <!-- Login Modal -->
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog">
-
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
+          <h4 style="color:red;"><span class="glyphicon glyphicon-lock"></span>LOGIN</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 style="color:red;"><span class="glyphicon glyphicon-lock"></span> Login</h4>
         </div>
         <div class="modal-body">
           <form role="form">
             <div class="form-group">
               <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
               <input type="text" class="form-control" id="usrname" placeholder="Enter Username">
-            </div>
+            </div>  
             <div class="form-group">
               <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
-              <input type="text" class="form-control" id="psw" placeholder="Enter password">
+              <input type="password" class="form-control" id="psw" placeholder="Enter password">
             </div>
-             <div class="checkbox">
-            <label><input type="checkbox" value="" checked>Remember me</label>
-          </div>
             <div class="modal-footer">
-            <button type="submit" class="btn btn-default btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+            <button type="submit" class="button btn-block" style="button"><span class="glyphicon glyphicon-off"></span> Login</button>
           </div>
           </form>
-          <button type="" class="btn btn-default btn-info pull-left" data-dismiss="modal">Sign up!</button>
-          <button type="" class="btn btn-default btn-warning pull-right" data-dismiss="modal">Forgot Password?</button>
+          <button type="button" class="button col-sm-4" style="button" data-target="#myModal2">Sign up instead</button>
+          <button type="" class="button col-sm-4" style="button" data-dismiss="modal">Forgot Password</button>
         </div>
       </div>
     </div>
   </div> 
+   <!-- Registry Modal -->
+  <div class="modal fade" id="myModal2" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 style="color:red;"><span class="glyphicon glyphicon-lock"></span>REGISTER</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <form role="form">
+            <div class="form-group">
+              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+              <input type="text" class="form-control" id="usrname" placeholder="Enter Username">
+            </div>  
+             <div class="form-group">
+              <label for="email"><span class="glyphicon glyphicon-email"></span> Email</label>
+              <input type="text" class="form-control" id="email" placeholder="Enter Email">
+            </div> 
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <input type="password" class="form-control" id="psw" placeholder="Enter password">
+            </div>
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <input type="password" class="form-control" id="psw2" placeholder="Retype password">
+            </div>
+            <div class="modal-footer">
+            <button type="submit" class="btn btn-default btn-success btn-block"><span class="glyphicon glyphicon-off"></span>Register me!</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
-<script>
-$(document).ready(function(){
-    $("#myBtn").click(function(){
-        $("#myModal").modal();
-    });
-});
-</script>
