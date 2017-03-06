d34a007c051f3a18ecd0ca4d5d046a2c164c3432
Erik de Graaff <erik_de_graaff@hotmail.com>
2017-03-06 13:13:59 +0100

    Update login.php
    php code van register toegevoegd

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
               <input type="password" class="form-control" id="psw" placeholder="Enter password">
             </div>
             <div class="modal-footer">
-            <button type="submit" class="button col-sm-12" style="button"><span class="glyphicon glyphicon-off"></span> Login</button>
+            <button type="submit" id="idlogin" class="button col-sm-12" style="button"><span class="glyphicon glyphicon-off"></span> Login</button>
           </div>
           </form>
       </div>
@@ -54,7 +54,7 @@
               <input type="password" class="form-control" id="psw2" placeholder="Retype password">
             </div>
             <div class="modal-footer">
-            <button type="submit" class="btn btn-default btn-success btn-block"><span class="glyphicon glyphicon-off"></span>Register me!</button>
+            <button type="submit" id="idregister" class="btn btn-default btn-success btn-block"><span class="glyphicon glyphicon-off"></span>Register me!</button>
           </div>
           </form>
         </div>
@@ -62,4 +62,97 @@
     </div>
   </div>
 </div>
+
+<?php
+   
+   if (isset($_POST["idregister"]))
+   {
+      include("./connect_db.php");
+    
+      $user = ($_POST["user"]);
+      $email = ($_POST["email"]);
+      $password = ($_POST["psw"]);
+      $password2 = ($_POST["psw2"]);
+      
+    if (strcmp($_POST["psw"], $_POST["psw2"]))
+    {
+
+      $sql = "SELECT `email` FROM `users` WHERE `email` = '".$_POST["email"]."'";
+      $result = mysqli_query($conn, $sql);
+
+      $record =  mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+      $sql = "INSERT INTO `users` (`id`,
+                                   `user`,
+                                   `email`, 
+                                   `password`,
+                                   `User_role`)
+              VALUES             (NULL,
+                                  '".$user."',
+                                  '".$email."', 
+                                  '".$psw."',
+                                  "FOLLOWER")";
+      
+      // vuur de query af op de database via de verbinding $conn
+      $result = mysqli_query($conn, $sql);
+     // $last_id = mysqli_insert_id($conn);
+
+      //als de query correct is ontvangen en uitgevoerd
+      if ($result)
+      {
+        $emailadress = $_POST["email"];
+        $subject = "Activatie account";
+     
+       $messageHtml = "<!DOCTYPE html>
+                        <html>
+                        <head>
+                        <style>
+                        body
+                        {
+                            font-family: Verdana, Arial;
+                            font-size: 1em;
+                            color: rgb(30, 0, 255);
+                        }
+
+                        </style>
+                        </head>
+                        <body>
+                        <h3>Beste ". $user.", </h3>".
+                        "<p>Bedankt voor het registreren, klik op onderstaande link</p>".
+                        "<p><a href='http://localhost/AM1A/LoginRegistration/index.php?content=activate&id=".
+                        $last_id."&pw=".$password."'>Activatie</a></p><p> om uw account te activeren</p>".
+                        "<p>Met vriendelijke groet,</p>".
+                        "Admin
+                        </body>
+                        </html>";           
+
+        $headers = "Cc: megan.j.online@gmail.com, erikgraaff@gmail.com"."\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8";
+        mail($emailadress,  $subject, $messageHtml, $headers);
+      
+         echo "U bent geregistreerd";
+         header("refresh:3; url=./index.php?content=home");
+
+         $sql = "INSERT INTO `fonts` AND `chapter_read` (`user`)
+
+                 VALUES                                 ('".$user."')";
+
+      }
+      else
+      {
+          //wanneer de query niet goed is ontvangen of uitgevoerd, meldt dit en stuur door
+         echo "Het emailadres is al bekend in onze database. Kies een nieuw mailadres";
+         header("refresh:3; url=./index.php?content=register_form");
+      }
+
+   }
+   else
+   {
+    echo"De door u ingevoerde wachtwoorden komen niet overeen. Probeer het nog een keer";
+    header("refresh: 3; url=./index.php?content=register_form");
+
+   }
+}
+?>
+
 
