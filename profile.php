9026dae503f4a33e5ae1d6b5de6c86d5afc01791
Erik de Graaff <erik_de_graaff@hotmail.com>
2017-04-03 20:44:46 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,21 +1,20 @@
 <?php
-  require("head.php");
+  require_once("head.php");
+  include_once("db.php");
 ?>
 <html>
   
  <head>
     <title>MY PROFILE</title>
 </head>
-  <body>
 
-<html>
 <body>
     <nav class="meny navbar-toggleable-md navbar navbar-right" style="height:200px;">
         <div class="container-fluid">
             <!-- Collect the nav links, forms, and other content for toggling -->
            
                 <ul class="nav navbar-nav">
-                    <li><button onclick="home()" class="button" style="button" id="comments">HOME</a></li>
+                    <li><button onclick="home()" class="button" style="button" id="home">HOME</li>
                     <li><button onclick="comments()" class="button" style="button" id="comments">COMMENTS</li>
                     <li><button onclick="chapters()" class="button" style="button" id="chapters">CHAPTERS</li>
                     <li><button onclick="style()" class="button" style="button" id="style">STYLE</li>
@@ -27,24 +26,29 @@
 </body>
 <script>
 
-          function home(){
-            window.location.href = "/index.php";
-          }
-          
+
+    function home(){
+        $(document).ready(function(){
+            $("#home").click(function(){
+
+            });
+        });
+    }
+
           function comments(){
           <?php 
-    include("db.php");
+
     // $user = $_POST['user'];
     // $connect = dblogin();
     // $sql = "SELECT * FROM comment WHERE user = ".$user;
     // mysqli_query($connect, $sql);
     // $result = mysqli_query($connect, $sql);
 
-$db = new DB();
-$db->selectComment('test43');
 
+    while ( $records = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
+         { 
     echo "<center>
-            <p>An overview of your comments:</p>
+            <p>These Are The Best Players In Tha World Biatch!!!</p>
                 <table>
                     <tr>
                         <th>Post Date</th>
@@ -62,17 +66,44 @@ $db->selectComment('test43');
          }
 ?>
           }
+          function chapters () {
+              document.getElementById('chapters').innerHTML = 'CHAPTERS';
 
-          function chapters(){
-            //pull read chapters from database, display, allow toggling and navigating
+              <?php
+              //pull read chapters from database, display, allow toggling and navigating
+              while ($records = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                  echo "
+            
+            <center>
+            <p>These Are The Best Chapters Of The World</p>
+                <table>
+                    <tr>
+                        <th>Post Date</th>
+                        <th>Chapter</th>
+                        
+                    </tr>
+                    <tr>
+                        <td>" . $records['date'] . "</td>
+                        <td>" . $records['chapter_read'] . "</td>
+                        
+                    </tr>
+                </table>
+            </center>
+         ";
+              }
+              ?>
           }
-
           function style(){
             //pull user css settings, allow modification
           }
 
+          <?php
+          echo'       
+          document.getElementById("logoff").innerHTML = "LOG<br>OUT";
           function logoff(){
-            // prompt logoff 
+              window.location.href = "logout.php";
           }
+          ';
+          ?>
 </script>
- </body>
+</html>
